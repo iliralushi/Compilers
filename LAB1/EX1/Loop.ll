359b9b01e9faeaa5eb76d261132c7df087f089db
iliralushi <iliralushi@gmail.com>
2025-03-19 21:17:37 +0100

    -

---
+++ LAB1/EX1/Loop.ll
@@ -0,0 +1,47 @@
+; ModuleID = '/home/ilir/Desktop/Ilir/School/Compilers/LAB1/Loop.c'
+source_filename = "/home/ilir/Desktop/Ilir/School/Compilers/LAB1/Loop.c"
+target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-pc-linux-gnu"
+
+@g = dso_local local_unnamed_addr global i32 0, align 4
+
+; Function Attrs: mustprogress nofree norecurse nosync nounwind willreturn memory(readwrite, argmem: none, inaccessiblemem: none) uwtable
+define dso_local i32 @g_incr(i32 noundef %0) local_unnamed_addr #0 {
+  %2 = load i32, ptr @g, align 4, !tbaa !5
+  %3 = add nsw i32 %2, %0
+  store i32 %3, ptr @g, align 4, !tbaa !5
+  ret i32 %3
+}
+
+; Function Attrs: mustprogress nofree norecurse nosync nounwind willreturn memory(readwrite, argmem: none, inaccessiblemem: none) uwtable
+define dso_local i32 @loop(i32 noundef %0, i32 noundef %1, i32 noundef %2) local_unnamed_addr #0 {
+  %4 = load i32, ptr @g, align 4, !tbaa !5
+  %5 = icmp slt i32 %0, %1
+  br i1 %5, label %6, label %10
+
+6:                                                ; preds = %3
+  %7 = sub i32 %1, %0
+  %8 = mul i32 %7, %2
+  %9 = add i32 %4, %8
+  store i32 %9, ptr @g, align 4, !tbaa !5
+  br label %10
+
+10:                                               ; preds = %6, %3
+  %11 = phi i32 [ %9, %6 ], [ %4, %3 ]
+  ret i32 %11
+}
+
+attributes #0 = { mustprogress nofree norecurse nosync nounwind willreturn memory(readwrite, argmem: none, inaccessiblemem: none) uwtable "min-legal-vector-width"="0" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
+
+!llvm.module.flags = !{!0, !1, !2, !3}
+!llvm.ident = !{!4}
+
+!0 = !{i32 1, !"wchar_size", i32 4}
+!1 = !{i32 8, !"PIC Level", i32 2}
+!2 = !{i32 7, !"PIE Level", i32 2}
+!3 = !{i32 7, !"uwtable", i32 2}
+!4 = !{!"Ubuntu clang version 19.1.7 (++20250114103320+cd708029e0b2-1~exp1~20250114103432.75)"}
+!5 = !{!6, !6, i64 0}
+!6 = !{!"int", !7, i64 0}
+!7 = !{!"omnipotent char", !8, i64 0}
+!8 = !{!"Simple C/C++ TBAA"}
---
+++ LAB1/EX2/TestPass.cpp
@@ -0,0 +1,96 @@
+//=============================================================================
+// FILE:
+//    TestPass.cpp
+//
+// DESCRIPTION:
+//    Visits all functions in a module and prints their names. Strictly speaking, 
+//    this is an analysis pass (i.e. //    the functions are not modified). However, 
+//    in order to keep things simple there's no 'print' method here (every analysis 
+//    pass should implement it).
+//
+// USAGE:
+//    New PM
+//      opt -load-pass-plugin=<path-to>libTestPass.so -passes="test-pass" `\`
+//        -disable-output <input-llvm-file>
+//
+//
+// License: MIT
+//=============================================================================
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+//-----------------------------------------------------------------------------
+// TestPass implementation
+//-----------------------------------------------------------------------------
+// No need to expose the internals of the pass to the outside world - keep
+// everything in an anonymous namespace.
+namespace {
+
+// New PM implementation
+struct TestPass: PassInfoMixin<TestPass> {
+  // Main entry point, takes IR unit to run the pass on (&F) and the
+  // corresponding pass manager (to be queried if need be)
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+
+  errs() << "Function Name: " << F.getName() << "\n";
+	errs() << "Number of Arguments: " << F.arg_size() << "\n";
+
+  int FunctionCalls = 0;
+	int BasicBlocks = 0;
+	int Instructions = 0;
+
+	for (auto InitBB = F.begin(); InitBB != F.end(); ++InitBB)
+	{
+    BasicBlocks++;
+		for (auto InitInstruction = InitBB->begin(); InitInstruction != InitBB->end(); ++InitInstruction)
+    {
+			Instructions++;
+      if (CallInst *call_inst = dyn_cast<CallInst>(&F))
+        FunctionCalls++;
+    }
+	}
+
+  errs() << "Number of Function Calls: " << FunctionCalls << "\n";
+	errs() << "Number of Basic Blocks: " << BasicBlocks << "\n";
+	errs() << "Number of Instructions: " << Instructions << "\n";
+
+  	return PreservedAnalyses::all();
+}
+
+
+  // Without isRequired returning true, this pass will be skipped for functions
+  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+  // all functions with optnone.
+  static bool isRequired() { return true; }
+};
+} // namespace
+
+//-----------------------------------------------------------------------------
+// New PM Registration
+//-----------------------------------------------------------------------------
+llvm::PassPluginLibraryInfo getTestPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "TestPass", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "test-pass") {
+                    FPM.addPass(TestPass());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+// This is the core interface for pass plugins. It guarantees that 'opt' will
+// be able to recognize TestPass when added to the pass pipeline on the
+// command line, i.e. via '-passes=test-pass'
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getTestPassPluginInfo();
+}
